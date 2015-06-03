f5231f450ca1cfdb3fa64921edd307860cf37655
Jay Peterson <contact@jaypeterson.com>
2015-06-03 09:59:37 -0700

    Reinstalled imagemagic and pics are up.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,10 @@
 class Pin < ActiveRecord::Base
+
   belongs_to :user
-end
+
+  
+  has_attached_file :image, :styles => {:large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  end
 	
